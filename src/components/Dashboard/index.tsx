e5bc2c9ceecbee0ae64ad37e437a3122372b5a36
joaovitordmoraes <joaovitordmoraes@gmail.com>
2022-03-23 22:39:37 -0300

    Create Dashboard component

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import { Summary } from '../Summary'
+import { TransactionTable } from '../TransactionTable'
+
+import * as S from './styles'
+
+export function Dashboard() {
+    return (
+        <S.Container>
+            <Summary />
+            <TransactionTable />
+        </S.Container>
+    )
+}
